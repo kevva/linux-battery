f35c305ef7d415bbc20065e00166e4647acd4a11
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-24 19:38:13 +0200

    Travis doesn't have a battery

--- test.js
+++ test.js
@@ -2,11 +2,13 @@
 var test = require('ava');
 var linuxBattery = require('./');
 
-test(function (t) {
-	t.plan(2);
+if (!process.env.CI) {
+	test(function (t) {
+		t.plan(2);
 
-	linuxBattery(function (err, battery) {
-		t.assert(!err, err);
-		t.assert(battery, battery);
+		linuxBattery(function (err, battery) {
+			t.assert(!err, err);
+			t.assert(battery, battery);
+		});
 	});
-});
+}
