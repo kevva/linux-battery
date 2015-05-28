fcd99ead22fe1a3bf704cf627e41a06445367cb8
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-28 18:20:29 +0200

    Add support for multiple batteries

--- test.js
+++ test.js
@@ -4,11 +4,12 @@ var linuxBattery = require('./');
 
 if (!process.env.CI) {
 	test(function (t) {
-		t.plan(2);
+		t.plan(3);
 
 		linuxBattery(function (err, battery) {
 			t.assert(!err, err);
-			t.assert(battery, battery);
+			t.assert(battery.length, battery);
+			t.assert(battery[0].nativePath, battery[0].nativePath);
 		});
 	});
 }
